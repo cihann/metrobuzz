92877e457e00c7817c60f520ed12bf2c4662d49d
fatiherikli <fatih@hipo.biz>
2013-12-21 22:51:21 +0200

    Initialized angular app

---
+++ js/main.js
@@ -0,0 +1,22 @@
+angular.module('metrobuzz', ['ngRoute', 'firebase'])
+
+.value('fbURL', 'https://metrobuzz.firebaseio.com/')
+
+.factory('Stations', function($firebase, fbURL) {
+  return $firebase(new Firebase(fbURL));
+})
+
+.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      controller:'HomeController',
+      templateUrl:'home.html'
+    })
+    .otherwise({
+      redirectTo:'/'
+    });
+})
+
+.controller('HomeController', function($scope, Stations) {
+  $scope.stations = Stations;
+});
