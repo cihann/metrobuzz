b44e5ffb54324e945d9797cba2c9d1cdff3b3045
fatiherikli <fatih@hipo.biz>
2013-12-22 00:57:48 +0200

    Implemented firebase app

--- js/main.js
+++ js/main.js
@@ -1,22 +1,25 @@
 angular.module('metrobuzz', ['ngRoute', 'firebase'])
 
-.value('fbURL', 'https://metrobuzz.firebaseio.com/')
+    .value('fbURL', 'https://metrobuzz.firebaseio.com/')
 
-.factory('Stations', function($firebase, fbURL) {
-  return $firebase(new Firebase(fbURL));
-})
-
-.config(function($routeProvider) {
-  $routeProvider
-    .when('/', {
-      controller:'HomeController',
-      templateUrl:'home.html'
+    .factory('Stations', function ($firebase, fbURL) {
+        return $firebase(new Firebase(fbURL));
     })
-    .otherwise({
-      redirectTo:'/'
-    });
-})
 
-.controller('HomeController', function($scope, Stations) {
-  $scope.stations = Stations;
-});
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                controller: 'HomeController',
+                templateUrl: 'home.html'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    })
+
+    .controller('HomeController', function ($scope, Stations) {
+        $scope.stations = Stations;
+        $scope.rate = function (rating) {
+            $scope.stations.$child($scope.station).$add(rating);
+        };
+    });
